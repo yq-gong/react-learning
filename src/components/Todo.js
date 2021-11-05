195c5320045cc7e87a49109592994c346a78eed1
yq Gong <gong.yq17@gmail.com>
2021-11-05 13:30:37 +0000

    add a behaviour for clicking the button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,17 @@
 //  without capitalised T it will not work!
 export default function Todo(props) {
+  function deleteHandler() {
+    alert(`Clicked ${props.text}`);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-          <span>{props.content}</span>
-        <button className="btn">Delete</button>
+        <span>{props.content}</span>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
